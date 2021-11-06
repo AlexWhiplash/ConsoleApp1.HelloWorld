542c181017ad75cc277ac476976c393b376a28c1
amtih <amtih@W-PC>
2021-11-06 13:46:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1.HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1.HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello C#!!!");
+			//Console.ReadLine();
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Wakeup, Neo...");
+			Console.ReadKey();
+			Console.Clear();
+			Console.WriteLine("The Matrix has you...");
+			Console.ReadKey();
+			Console.Clear();
+			Console.WriteLine("Follow the white Rabbit...");
+			Console.ReadKey();
+			Console.Clear();
+			Console.WriteLine("Knock knock, Neo. ");
+			Console.ReadKey();
+
+
+
+
+		}
+	}
+}
